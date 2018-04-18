d25dcca67eac0246c69fc02ff33b1d90799c0318
KrasuckiD <darasmm@gmail.com>
2018-04-18 17:30:58 +0200

    fasade v2

--- src/main/java/com/sda/fasade/FilesTest.java
+++ src/main/java/com/sda/fasade/FilesTest.java
@@ -4,10 +4,11 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 public class FilesTest {
-//    public static void main(String[] args) throws FileNotFoundException {
-//        QuizFileRepository quizFileFasade = new QuizFileRepository();
-//       List<QuizQuestion> questions = quizFileFasade.
-//               readAll("C:\\Users\\Dell\\Desktop\\projekty\\javapoz7Patterns\\text.txt");
-//       questions.forEach(e -> System.out.println(e));
-//    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        QuizFileRepository quizFileRepository = new QuizFileRepository("test.txt");
+        List<QuizQuestion> questions = quizFileRepository.readAll();
+        questions.forEach(e -> System.out.println(e));
+        System.out.println("test");
+    }
 }
